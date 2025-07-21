99bc1c36d9f97ac0f0e3d49a6cae46433203c089
Asilbek Ismatilloev <66477122+aikkku@users.noreply.github.com>
2025-07-21 18:55:12 -0400

    1302-DELETE-CHARACTERS-TO-MAKE-FANCY-STRING Stats: Time: 28 ms (56.18%), Space: 44.4 MB (67.73%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string makeFancyString(string s) {
         int cur(1);
-        string ans = "";
+        string ans("");
         ans+=s[0];
         for(int i = 1; i < s.length(); i++) {
             if(s[i] == s[i-1]) {
