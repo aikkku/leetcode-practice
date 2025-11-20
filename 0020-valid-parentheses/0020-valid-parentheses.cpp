711b7825f3e7c36423d630cb000737adbe3f46d9
Asilbek Ismatilloev <66477122+aikkku@users.noreply.github.com>
2025-11-19 19:04:09 -0500

    0020-VALID-PARENTHESES Stats: Time: 0 ms (100%), Space: 9.1 MB (20.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,17 +3,12 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for(int i = 0; i < s.length(); i++) {
-            if(s[i] == '(' or s[i] == '[' or s[i] == '{') {
-                st.push(s[i]);
-            }else if(st.size() == 0) {
-                return false;
-            } else if(s[i] == ')' and st.top() == '(') {
-                st.pop();
-            } else if(s[i] == '}' and st.top() == '{') {
-                st.pop();
-            } else if(s[i] == ']' and st.top() == '[') {
-                st.pop();
-            } else return false;
+            if(s[i] == '(' or s[i] == '[' or s[i] == '{') st.push(s[i]);
+            else if(st.size() == 0) return false;
+            else if((s[i] == ')' and st.top() == '(') 
+                        or (s[i] == ']' and st.top() == '[') 
+                        or (s[i] == '}' and st.top() == '{')) st.pop();
+            else return false;
         }
         if(st.size() == 0) return true;
         return false;
