cf9f5c809d9f10e7ac21d80178ad38355f73acc0
Asilbek Ismatilloev <66477122+aikkku@users.noreply.github.com>
2025-12-20 09:46:06 +0500

    1020-LONGEST-TURBULENT-SUBARRAY Stats: Time: 1732 ms (5.1%), Space: 44 MB (84.83%) - LeetHub

--- 1020-longest-turbulent-subarray/1020-longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/1020-longest-turbulent-subarray.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        int ans = 1, cur = 1;
-        int id = 0;
+        bool a = 0;
+        int temp = 1, ans = 1;
+
         for(int i = 1; i < arr.size(); i++) {
-            if(arr[i] == arr[i-1]) {
-                cur = 1;
-            } else if(arr[i] > arr[i-1]) {
-                if(id == -1) {
-                    cur++;
-                } else cur = 2;
-                id = 1;
+            if(temp == 1 and arr[i] != arr[i - 1]) {
+                temp ++;
+                a = arr[i] < arr[i - 1];
+            }else if((!a and arr[i] < arr[i - 1]) or (a and arr[i] > arr[i - 1])) {
+                temp ++;
+                a = !a;
             } else {
-                if(id == 1) {
-                    cur++;
-                } else cur = 2;
-                id = -1;
+                ans = max(temp, ans);
+                temp = 1;
+                if(arr[i] != arr[i - 1]) i--;
             }
-
-            ans = max(cur, ans);
+            cout<<arr[i]<<' '<<temp<<endl;
         }
+        cout<<endl;
 
-        return ans;
+        return max(ans, temp);
     }
 };
