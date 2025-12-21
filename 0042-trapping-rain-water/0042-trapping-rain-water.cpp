2d75a4c455a42279b934f53fbbfa711f3bec24fb
Asilbek Ismatilloev <66477122+aikkku@users.noreply.github.com>
2025-12-21 16:28:03 +0500

    0042-TRAPPING-RAIN-WATER Stats: Time: 0 ms (100%), Space: 26.6 MB (45.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int trap(vector<int>& h) {
-        vector<int> suffix_sum(h.size());
-        int cmax = 0;
-        for(int i = h.size() - 1; i > -1; i--) {
-            cmax = max(cmax, h[i]);
-            suffix_sum[i] = cmax;
+        vector<int> suffix(h.size());
+        suffix[h.size() - 1] = h[h.size() - 1];
+        for(int i = h.size() - 2; i > -1; i--) {
+            suffix[i] = max(h[i], suffix[i + 1]);
         }
 
-        int c = 0;
-        int sum = 0;
-        for(int i = 0; i < h.size(); i++) {
-            if(h[i] > 0) {
-                c = max(c, h[i]);
-            }
-
-            sum += min(c, suffix_sum[i]) - h[i];
-
-
+        int sum = 0, c = 0;
+        for(int i = 0; i < h.size() - 1; i++) {
+            if(h[i] > c) c = h[i];
+            else sum += min(c, suffix[i]) - h[i];
         }
 
         return sum;
