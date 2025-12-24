692db2a7480ff5abe763811760c7401215884cea
Asilbek Ismatilloev <66477122+aikkku@users.noreply.github.com>
2025-12-24 14:00:17 +0500

    0783-SEARCH-IN-A-BINARY-SEARCH-TREE Stats: Time: 0 ms (100%), Space: 35.5 MB (61.27%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -12,9 +12,8 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(root == nullptr or root->val == val) {
-            return root;
-        } else if(root->val < val) return searchBST(root->right, val);
+        if(!root or root->val == val) return root;
+        if(val > root->val) return searchBST(root->right, val);
         else return searchBST(root->left, val);
     }
 };
