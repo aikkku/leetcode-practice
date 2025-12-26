d74bcbbf7b2ab21bca08cfb44cd8968bf96c2109
Asilbek Ismatilloev <66477122+aikkku@users.noreply.github.com>
2025-12-26 15:06:04 +0500

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 7.7 MB (86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n <= 2) return n;
+        int dp[2] = {1, 2};
 
-        vector<int> dp = {1, 2};
+        if(n < 3) return dp[n - 1];
+
         for(int i = 2; i < n; i++) {
             int temp = dp[1];
-            dp[1] += dp[0];
+            dp[1] = dp[0] + dp[1];
             dp[0] = temp;
         }
 
