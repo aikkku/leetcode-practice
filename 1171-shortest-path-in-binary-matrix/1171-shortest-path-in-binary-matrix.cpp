b198fb43008ccf9214aa1391f2a7472e51066f0f
Asilbek Ismatilloev <66477122+aikkku@users.noreply.github.com>
2026-01-03 21:09:59 +0500

    1171-SHORTEST-PATH-IN-BINARY-MATRIX Stats: Time: 147 ms (10.93%), Space: 36.8 MB (17.6%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,34 +1,51 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int r = 0, c = 0, rr = grid.size() - 1, cc = grid[0].size() - 1;
-        if(grid[0][0] == 1 or grid[rr][cc] == 1) return -1;
-
-        vector<pair<int,int>> d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, -1}, {1, 1}, {-1, -1}, {-1, 1}};
-
-        queue<pair<int,int>> q;
-        q.push({0, 0});
+        int r = 0, c = 0;
+        int rr = grid.size() - 1, cc = grid[0].size() - 1;
+        if(grid[r][c] == 1 or grid[rr][cc] == 1) return -1;
 
         int l = 1;
 
+        vector<pair<int, int>> dv = {
+            {1, 0}, {0, 1},
+            {-1, 0}, {0, -1},
+            {1, 1}, {-1, -1},
+            {1, -1}, {-1, 1},
+        };
+
+        set<pair<int, int>> visited;
+
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        visited.insert({0, 0});
         while(!q.empty()) {
-            int ql = q.size();
-            while(ql--) {
-                r = q.front().first;
-                c = q.front().second;
+            int temp_size = q.size();
+
+            while(temp_size--) {
+                int cur_r = q.front().first, 
+                    cur_c = q.front().second;
+                
                 q.pop();
 
-                if(r == rr and c == cc) return l;
+                if(cur_r == rr and cur_c == cc) {
+                    return l;
+                }
 
-                for(pair<int, int> p : d) {
-                    if(min(r + p.first, c + p.second) < 0 or r + p.first > rr or c + p.second > cc or grid[r + p.first][c + p.second] == 1) {
-                        continue;
-                    }
-                    q.push({r + p.first, c + p.second});
-                    grid[r + p.first][c + p.second] = 1;
+                for(const pair<int, int>& d : dv) {
+                    int new_r = cur_r + d.first,
+                        new_c = cur_c + d.second;
+
+                    if(min(new_r, new_c) < 0 
+                        or new_r > rr or new_c > cc
+                        or visited.count({new_r, new_c})
+                        or grid[new_r][new_c] == 1) continue;
+
+                    q.push({new_r, new_c});
+                    visited.insert({new_r, new_c});
                 }
             }
-            
+
             l++;
         }
 
