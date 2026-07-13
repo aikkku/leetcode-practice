c8a73a68856c1398d31f21f5d35b66780e9185b7
Asilbek Ismatilloev <aismatil@u.rochester.edu>
2026-07-12 21:01:37 -0400

    0049-GROUP-ANAGRAMS Stats: Time: 11 ms (91.61%), Space: 25.9 MB (57.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,29 +2,19 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
-
         string temp;
-        for(string s : strs) {
-            temp = s;
+        for(int i = 0; i < strs.size(); i ++) {
+            temp = strs[i];
             sort(temp.begin(), temp.end());
-
-            mp[temp].push_back(s);
+            mp[temp].push_back(strs[i]);
         }
 
         vector<vector<string>> ans;
-        vector<string> tv;
-
-        for(auto it = mp.begin(); it != mp.end(); it++) {
-            tv.clear();
-            
-            for(string s : it->second) {
-                tv.push_back(s);
-            }
-
-            ans.push_back(tv);
+        for(auto it : mp) {
+            ans.push_back(it.second);
         }
 
         return ans;
-
+        
     }
 };
