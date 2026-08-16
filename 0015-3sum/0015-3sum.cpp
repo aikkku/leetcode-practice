699f750a72d371e405002ef59c7312878e8c0587
Asilbek Ismatilloev <aismatil@u.rochester.edu>
2026-08-16 03:28:03 -0400

    0015-3SUM Stats: Time: 44 ms (74.5%), Space: 29.1 MB (46.81%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int L, R = nums.size() - 1;
-
         vector<vector<int>> ans;
-        for(int i = 0; i < nums.size() - 2; i++) {
-            if(i != 0 and nums[i] == nums[i - 1]) continue;
-            L = i + 1;
-            R = nums.size() - 1;
-            while(L < R) {
-                if(nums[L] + nums[R] == -nums[i]) {
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int L = i + 1;
+            int R = nums.size() - 1;
+
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+
+                if (sum == 0) {
                     ans.push_back({nums[i], nums[L], nums[R]});
-                    R--;
-                    while(L < R and nums[R] == nums[R + 1]) R--;
+
                     L++;
-                    while(L < R and nums[L] == nums[L - 1]) L++;
+                    R--;
+
+                    while (L < R && nums[L] == nums[L - 1])
+                        L++;
+
+                    while (L < R && nums[R] == nums[R + 1])
+                        R--;
+
+                } else if (sum < 0) {
+                    L++;
+                } else {
+                    R--;
                 }
-                else if(nums[L] + nums[R] < -nums[i]) L++;
-                else R--;
             }
         }
 
